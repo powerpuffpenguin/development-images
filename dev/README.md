c233eef9432b29eae94f4c817bd5b95f1e64805b
zuiwuchang <zuiwuchang@gmail.com>
2023-02-15 10:57:06 +0800

    node 18.14

--- dev/README.md
+++ dev/README.md
@@ -3,3 +3,12 @@
 這裏爲開發環境 打包了 基礎鏡像
 
 主要是安裝了一些通用的工具和設定
+
+```
+apt-get install -y --no-install-recommends \
+    gosu \
+    tzdata ca-certificates bash-completion net-tools iputils-ping \
+    upx zip unzip xz-utils p7zip-full \
+    dnsutils vim git \
+    sudo proxychains curl wget
+```
---
+++ node/tools_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# 設置顯示的命令名稱
+CommandName="tools_docker.sh"
+
+# 項目根路徑
+cd `dirname $BASH_SOURCE`
+ProjectDir=`pwd`
+
+# 設置環境
+Docker="king011/dev-node" # 發佈名稱
+DefaultTag=18.14.0 # 默認版本
+Dockerfile="`pwd`/dockerfile" # 設置 dockerfile 檔案夾
+
+# 加載腳本
+source ../command/tools.sh
+# 調用腳本
+main "${@}"
