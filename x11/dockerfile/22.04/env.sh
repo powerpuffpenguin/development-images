2941fb045f9fcad9b87efe904f7e5b871642e748
selene201226 <selene201226@gmail.com>
2025-02-20 07:02:43 +0800

    rime

--- x11/dockerfile/22.04/env.sh
+++ x11/dockerfile/22.04/env.sh
@@ -1,2 +1,18 @@
 DockerVarName="test-ubuntu-22.04"
 DockerVarUser="abc"
+
+# * $1 images
+# * $2 name
+function docker_run
+{
+    sudo docker run --rm \
+        -it \
+        --name "$2" \
+        -e TZ=Asia/Shanghai \
+        -e LANG=C.UTF-8 \
+        -e LC_ALL=C.UTF-8 \
+        -e SCREEN_MODE=1024x768x24 \
+        -v /home/king/project:/home/abc/project \
+        -p 8080:80 \
+        "$1"
+}
